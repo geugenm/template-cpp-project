2418b0ef2ae7d2da1bd4020130dc6e67c7a26b24
e_gleba <i@egleba.ru>
2025-02-01 20:05:51 +0300

    small fixes

--- cmake/pkg/imgui-config.cmake
+++ cmake/pkg/imgui-config.cmake
@@ -9,26 +9,23 @@ cpmaddpackage(
     DOWNLOAD_ONLY
     TRUE)
 
-# CMakeLists.txt from https://gist.githubusercontent.com/rokups/f771217b2d530d170db5cb1e08e9a8f4
-file(
-    DOWNLOAD
-    "https://gist.githubusercontent.com/rokups/f771217b2d530d170db5cb1e08e9a8f4/raw/4c2c14374ab878ca2f45daabfed4c156468e4e27/CMakeLists.txt"
-    "${imgui_SOURCE_DIR}/CMakeLists.txt"
-    EXPECTED_HASH
-        SHA256=fd62f69364ce13a4f7633a9b50ae6672c466bcc44be60c69c45c0c6e225bb086)
+add_library(imgui STATIC)
 
-# Options
-set(IMGUI_DEMO TRUE)
-set(IMGUI_ENABLE_STDLIB_SUPPORT TRUE)
-set(IMGUI_IMPL_SDL FALSE)
-set(IMGUI_IMPL_GLFW FALSE)
-set(IMGUI_IMPL_GLUT FALSE)
-set(IMGUI_IMPL_VULKAN FALSE)
+target_sources(
+    imgui
+    PRIVATE ${imgui_SOURCE_DIR}/imgui.cpp
+            ${imgui_SOURCE_DIR}/imgui_demo.cpp
+            ${imgui_SOURCE_DIR}/imgui_draw.cpp
+            ${imgui_SOURCE_DIR}/imgui_tables.cpp
+            ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+            ${imgui_SOURCE_DIR}/misc/cpp/imgui_stdlib.cpp
+            ${imgui_SOURCE_DIR}/misc/freetype/imgui_freetype.cpp)
 
-# Add subdirectory
-add_subdirectory(${imgui_SOURCE_DIR} EXCLUDE_FROM_ALL SYSTEM)
+target_include_directories(
+    imgui SYSTEM PUBLIC ${imgui_SOURCE_DIR} ${imgui_SOURCE_DIR}/misc/cpp
+                        ${imgui_SOURCE_DIR}/misc/freetype)
 
-target_include_directories(imgui SYSTEM INTERFACE ${imgui_SOURCE_DIR})
+target_link_libraries(imgui PRIVATE freetype)
 
 # Adding backend implementations
 #
@@ -37,8 +34,6 @@ target_include_directories(imgui SYSTEM INTERFACE ${imgui_SOURCE_DIR})
 #   - Fedora:    sudo dnf install mesa-libGL-devel mesa-libGLU-devel
 #   - Arch:      sudo pacman -S mesa glu
 #   - Ubuntu:    sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev
-
-target_link_libraries(imgui INTERFACE freetype)
 
 add_library(imgui_sdl3_opengl3 STATIC)
 
