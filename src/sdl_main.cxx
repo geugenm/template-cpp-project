55e416f399c862db12129593371577ee8727acbf
e_gleba <i@egleba.ru>
2025-06-02 00:16:27 +0300

    feat(sdl): Add SDL initialization and message box example

--- src/sdl_main.cxx
+++ src/sdl_main.cxx
@@ -1,36 +1,33 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-
-/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
-/*                                                       */
-/* Remove this source, and replace with your SDL sources */
-/*                                                       */
-/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+#include <cstdlib>
 
 int main(int argc, char* argv[])
 {
-    (void)argc;
-    (void)argv;
     if (!SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO))
     {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "SDL_Init failed (%s)",
+                     "SDL_Init failed: %s",
                      SDL_GetError());
-        return 1;
+        return EXIT_FAILURE;
     }
 
-    if (!SDL_ShowSimpleMessageBox(
+    struct SDLGuard
+    {
+        ~SDLGuard() { SDL_Quit(); }
+    } sdl_guard;
+
+    if (SDL_ShowSimpleMessageBox(
             SDL_MESSAGEBOX_INFORMATION,
             "Hello World",
             "!! Your SDL project successfully runs on Android !!",
-            NULL))
+            nullptr) != 0) // Prefer nullptr over NULL
     {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "SDL_ShowSimpleMessageBox failed (%s)",
+                     "Message box failed: %s",
                      SDL_GetError());
-        return 1;
+        return EXIT_FAILURE;
     }
 
-    SDL_Quit();
-    return 0;
+    return EXIT_SUCCESS;
 }
