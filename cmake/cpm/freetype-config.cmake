0033818cac4466f9fb8e12919490247403334d30
e_gleba <i@egleba.ru>
2025-06-01 19:53:18 +0300

    Update GitHub Actions workflow for CMake multi-platform builds

--- cmake/cpm/freetype-config.cmake
+++ cmake/cpm/freetype-config.cmake
@@ -5,20 +5,16 @@ cpmaddpackage(
     VERSION 2.10.0
 )
 
-# Apply RC flags only for MSVC
 set_target_properties(
     freetype
     PROPERTIES
         CMAKE_RC_FLAGS "$<$<C_COMPILER_ID:MSVC>:${CMAKE_RC_FLAGS} /c65001>"
 )
 
-# Apply compile options only for MSVC (note: RC flags are for resource compiler, not C/C++)
-# If you meant to add compile options for MSVC only, use:
 target_compile_options(freetype PRIVATE "$<$<C_COMPILER_ID:MSVC>:/c65001>")
 # Or, if you want to append, use a generator expression to only apply for MSVC:
 # target_compile_options(freetype PRIVATE "$<$<C_COMPILER_ID:MSVC>:${CMAKE_CXX_FLAGS} /c65001>")
 
-# Apply definition only for MSVC
 target_compile_definitions(freetype PRIVATE "$<$<C_COMPILER_ID:MSVC>:_UNICODE>")
 
 add_library(Freetype::Freetype ALIAS freetype)
